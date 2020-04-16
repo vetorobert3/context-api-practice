08cf9fb9a51c6ebba42636d984c4b67d194eacc6
Veto Ramirez <vetorobert3@gmail.com>
2020-04-15 20:03:20 -0700

    Finished project for a simple context API.

--- context-prac/src/App.js
+++ context-prac/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import MovieList from './MovieList';
 import Nav from './Nav';
 import { MovieListProvider } from './MovieListContext';
+import AddMovie from './AddMovie';
 
 function App() {
   return (
     <MovieListProvider>
       <div className="App">
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieListProvider>
--- context-prac/src/Nav.js
+++ context-prac/src/Nav.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { MovieListContext } from './MovieListContext';
 
 const Nav = () => {
+  const [movies, setMovies] = useContext(MovieListContext);
 
   return (
     <div>
       <h3>Context Prac</h3>
-      <p>List of Movies: </p>
+      <p>List of Movies: {movies.length}</p>
     </div>
   );
 }
