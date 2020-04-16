756e0edc07fd1ef7d040ed86e9e253344c59c408
Veto Ramirez <vetorobert3@gmail.com>
2020-04-15 18:30:29 -0700

    Created a react app, two components (MovieList and Movies)

---
+++ context-prac/src/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Movie = () => {
+
+  return (
+    <div>
+      <h3></h3>
+    </div>
+
+  );
+}
+
+export default Movie; 
---
+++ context-prac/src/MovieList.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const MovieList = () => {
+  const [movies, setMovies] = useState([
+    {
+      name: 'Harry Potter',
+      price: '$10',
+      id: 23124
+    },
+    {
+      name: 'Game of Thrones',
+      price: '$10',
+      id: 2566124
+    },
+    {
+      name: 'Inception',
+      price: '$10',
+      id: 23524
+    }
+  ]);
+  return (
+    <div>
+      {movies.map(movie => (
+        <li>{movie.name}</li>
+      ))}
+    </div>
+
+  );
+}
+
+export default MovieList; 
